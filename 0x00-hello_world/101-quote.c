151c3038656737e1155fc9148ab00b683ffa91b9
Collins <astutecollins4law@yahoo.com>
2023-03-16 16:00:56 -0700

    done!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+#include<unistd.h>int main(){const char*msg="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";write(2,msg,54);return 1;}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+#include <stdio.h> int main() { printf("With proper grammar, but the outcome is a piece of art,\n"); return 0; }
